df235db95a546c048d0b100296bb72ebb6cf1a18
leaoereno <rafael.ereno@outlook.com>
2026-06-14 16:26:37 -0300

    fix: ocultar menu para usuários com perfil User (type=1)

--- Module.php
+++ Module.php
@@ -6,11 +6,20 @@ use Zabbix\Core\CModule;
 use APP;
 use CMenuItem;
 use CView;
+use CWebUser;
 
 class Module extends CModule {
 
     public function init(): void {
         CView::registerDirectory(__DIR__ . '/views');
+
+        // Exibe o menu apenas para Admin (type=2) e Super Admin (type=3).
+        // Usuários com perfil User (type=1) não devem ver estas opções.
+        $userType = (int) CWebUser::$data['type'];
+
+        if (!in_array($userType, [USER_TYPE_ZABBIX_ADMIN, USER_TYPE_SUPER_ADMIN])) {
+            return;
+        }
 
         APP::Component()->get('menu.main')
             ->findOrAdd(_('Users'))
